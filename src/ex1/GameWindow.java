434258249d5c8f666234f340de169bfac8db2e2d
KinhLupNe <163908536+KinhLupNe@users.noreply.github.com>
2024-12-27 00:41:04 +0700

    getting

---
+++ src/ex1/GameWindow.java
@@ -0,0 +1,16 @@
+package ex1;
+
+import javax.swing.*;
+
+public class GameWindow {
+    private JFrame jframe;
+
+    public  GameWindow(GamePanel gamePanel){
+        jframe = new JFrame();
+
+        jframe.setSize(400, 400);
+        jframe.add(gamePanel);
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+    }
+}
